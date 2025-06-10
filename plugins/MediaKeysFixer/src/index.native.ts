19561a053d7d0ee281295b5905f02dffef97b2e9
Aztup <38fight.gaming06@gmail.com>
2025-06-10 05:04:57 +0200

    fix: plugin not unloading properly

--- plugins/MediaKeysFixer/src/index.native.ts
+++ plugins/MediaKeysFixer/src/index.native.ts
@@ -1,19 +1,24 @@
 import {globalShortcut, app} from 'electron';
+import type { LunaUnload } from '@luna/core';
 export type PlayStates = 'next' | 'previous' | 'pause' | 'playPause' | 'play';
+
+export const unloads = new Set<LunaUnload>();
 
 app.whenReady().then(() => {
     globalShortcut.unregisterAll();
-    
+
     const actions = [
         ['MediaNextTrack', 'next'],
         ['MediaPreviousTrack', 'previous'],
         ['MediaStop', 'pause'],
         ['MediaPlayPause', 'playPause'],
     ]
-    
+
     for (const [accelerator, method] of actions) {
         globalShortcut.register(accelerator, () => {
             luna.tidalWindow?.webContents.send('TidalFixer:callMethod', method);
         });
+
+        unloads.add(() => globalShortcut.unregister(accelerator))
     }
 });
--- plugins/MediaKeysFixer/src/index.ts
+++ plugins/MediaKeysFixer/src/index.ts
@@ -1,6 +1,9 @@
 import './index.native';
-import { ipcRenderer, PlayState, unloads } from "@luna/lib";
+import { ipcRenderer, PlayState } from "@luna/lib";
+import type { LunaUnload } from '@luna/core';
 import type { PlayStates } from './index.native';
+
+export const unloads = new Set<LunaUnload>();
 
 ipcRenderer.on(unloads, 'TidalFixer:callMethod', (name: PlayStates) => {
     if (name === 'playPause') {
