54f11ac5faf966dbede9224b17da3c929f120c91
Aztup <38fight.gaming06@gmail.com>
2025-06-10 05:23:41 +0200

    fix previous not using the same method as tidal

--- plugins/MediaKeysFixer/src/index.ts
+++ plugins/MediaKeysFixer/src/index.ts
@@ -1,5 +1,5 @@
 import './index.native';
-import { ipcRenderer, PlayState } from "@luna/lib";
+import { ipcRenderer, PlayState, redux } from "@luna/lib";
 import type { LunaUnload } from '@luna/core';
 import type { PlayStates } from './index.native';
 
@@ -8,6 +8,9 @@ export const unloads = new Set<LunaUnload>();
 ipcRenderer.on(unloads, 'TidalFixer:callMethod', (name: PlayStates) => {
     if (name === 'playPause') {
         name = PlayState.playing ? 'pause' : 'play';
+    } else if (name === 'previous') {
+        redux.actions['playbackControls/SKIP_PREVIOUS']();
+        return;
     }
 
     PlayState[name]();    
