3362cf4f602fdfd56ae9c69736191b330470085b
Aztup <38fight.gaming06@gmail.com>
2025-06-11 14:49:47 +0200

    Fix unload

--- plugins/DeviceDisconnectFix/src/index.ts
+++ plugins/DeviceDisconnectFix/src/index.ts
@@ -1,7 +1,7 @@
 import { LunaUnload } from "@luna/core";
 import { redux, ipcRenderer } from "@luna/lib";
 
-const unloads = new Set<LunaUnload>();
+export const unloads = new Set<LunaUnload>();
 
 ipcRenderer.on(unloads, 'client.playback.playersignal', (args)=>{
     if ('devices' in args){ // New devices have been added, probably should listen for the other thing though...
